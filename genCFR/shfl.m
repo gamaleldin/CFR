b47c9f4d5de88a35cded0e19d768a71337836b48
gamaleldin <gamaleldin.elsayed@gmail.com>
2017-01-09 20:07:58 -0500

    add shuffling function

---
+++ genCFR/shfl.m
@@ -0,0 +1,57 @@
+% origTensor: original tensor
+% shflAcross: a boolean vector of 3 elements, if first elemeny=t is true
+% the shuffle is performed across times, if second element is true the
+% shuffle is performed across neurons, if third element is true the shuffle
+% is performed across conditions. Multiple shuffles are allowed. for
+% example, if second and third elements are both true, the shuffle is
+% performed across neurons and conditions.
+% cyclicShfl: if true the shuffle is performed cyclicly.
+%%              
+function shuffledTensor = shfl(origTensor, shflAcross, cyclicShfl)
+[T, N, C] = size(origTensor);
+shuffledTensor = origTensor;
+
+%% shuffle across times
+if shflAcross(1)
+    parfor n = 1:N
+        if cyclicShfl
+            startTime = randi(T);
+            ts = [startTime:T, 1:startTime-1];
+        else
+            ts = randperm(T);
+        end
+        A = shuffledTensor(:, n, :);
+        A = A(ts, 1, :);  
+        shuffledTensor(:, n, :) = A;
+    end
+end
+%%
+if shflAcross(2)
+    parfor c = 1:C
+        if cyclicShfl
+            startNeu = randi(N);
+            ns = [startNeu:N, 1:startNeu-1];
+        else
+            ns = randperm(N);
+        end
+        A = shuffledTensor(:, :, c);
+        A = A(:, ns, 1);  
+        shuffledTensor(:, :, c) = A;
+    end
+end
+%% shuffle across conditions
+if shflAcross(3)
+    parfor n = 1:N
+        if cyclicShfl
+            startCond = randi(C);
+            cs = [startCond:C, 1:startCond-1];
+        else
+            cs = randperm(C);
+        end
+        A = shuffledTensor(:, n, :);
+        A = A(:, 1, cs);  
+        shuffledTensor(:, n, :) = A;
+    end
+end
+
+end
