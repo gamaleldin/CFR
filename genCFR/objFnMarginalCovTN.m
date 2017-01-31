796d31a20300612be315c31bf900c62cf42c7c0e
gamaleldin <gamaleldin.elsayed@gmail.com>
2017-01-31 14:58:04 -0500

    use the new objFns

--- genCFR/objFnMarginalCovTN.m
+++ genCFR/objFnMarginalCovTN.m
@@ -3,8 +3,14 @@ surrTensor = varargin{1};
 targetSigma_T = varargin{2};
 targetSigma_N = varargin{3};
 
-[fT, ~, gradfT_unc] = objFnMarginalCovT(K, surrTensor, targetSigma_T);
-[fN, ~, gradfN_unc] = objFnMarginalCovN(K, surrTensor, targetSigma_N);
+[fT, ~, gradfT_unc] = objFnMarginalCovOther(K, permute(surrTensor, [2, 1, 3]), targetSigma_T);
+[fN, ~, gradfN_unc] = objFnMarginalCovReadOut(K, permute(surrTensor, [2, 1, 3]), targetSigma_N);
+
+
+% [fT, ~, gradfT_unc] = objFnMarginalCovT(K, surrTensor, targetSigma_T);
+% [fN, ~, gradfN_unc] = objFnMarginalCovN(K, surrTensor, targetSigma_N);
+
+
 f = (fT+fN)/2;
 gradf = projEigSpace((gradfT_unc+gradfN_unc)./2, ones(size(K,2),1));
 end
